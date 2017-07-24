47a661b7f21b1ce634ab0da3bcc88b9485349b78
torstenzwoch <torstenzwoch@outlook.com>
2017-07-24 17:46:10 +0200

    Code angepasst

--- js/contact_me.js
+++ js/contact_me.js
@@ -19,7 +19,7 @@ $(function () {
             event.preventDefault();
             var email = $("input#subscribe-email").val();
             $.ajax({
-                url: "https://softwarepioniere-functions.azurewebsites.net/api/NewsletterAnmeldung?code=wybtF1irV0OagSTOC2Fe8H4vWgl6Wkf678If5nUiPfALEQdViJlnMg", method: "POST", dataType: "json", data: { "email": email, "status": "pending", "merge_fields": { "FNAME": "", "LNAME": "" } }, cache: false,
+                url: "https://softwarepioniere-functions.azurewebsites.net/api/NewsletterAnmeldung", method: "POST", dataType: "json", data: { "code": "wybtF1irV0OagSTOC2Fe8H4vWgl6Wkf678If5nUiPfALEQdViJlnMg==", "email": email}, cache: false,
                 success: function () { 
                     $('#success-newsletter').html("<div class='alert alert-success'>"); 
                     $('#success-newsletter > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>"); $('#success-newsletter > .alert-success').append("<strong>Ihre E-Mail Adresse wurde erfolgreich für unseren Newsletter eingetragen.</strong>"); $('#success-newsletter > .alert-success').append('</div>'); $('#mc-embedded-subscribe-form').trigger("reset"); }, error: function () { $('#success-newsletter').html("<div class='alert alert-danger'>"); $('#success-newsletter > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>"); $('#success-newsletter > .alert-danger').append("<strong>Ihre Eintragung zum Newsletter konnte nicht durchgeführt werden. Probieren Sie es bitte später noch einmal.</strong>"); $('#success-newsletter > .alert-danger').append('</div>'); $('#mc-embedded-subscribe-form').trigger("reset"); },
