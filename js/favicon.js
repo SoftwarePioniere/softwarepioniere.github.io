7c635377d8232fb306c94816654d7405ee31af36
Torsten Zwoch <tz@softwarepioniere.de>
2022-06-03 10:16:02 +0200

    change favicon dark/light

--- js/favicon.js
+++ js/favicon.js
@@ -1,16 +1,13 @@
 lightSchemeIcon = document.querySelector('link#light-scheme-icon');
 darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
-favicon = document.querySelector('link#favicon');
 
 function onUpdate() {
     if (matcher.matches) {
-        favicon.remove();
         lightSchemeIcon.remove();
         document.head.append(darkSchemeIcon);
     } else {
         document.head.append(lightSchemeIcon);
         darkSchemeIcon.remove();
-        favicon.remove();
     }
 }
 
